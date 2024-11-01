e4735cab42ba17cf7c8c7bc39bde8cf9161898ea
sergeis146 <sergei.surkov@stu.ocsb.ca>
2024-11-01 10:09:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 let direction;
 let churchChoice;
 let graveChoice;
+let weapon;
+let outcome;
 //main game code
 function startGame() {
     let name = prompt("What is your name?");
@@ -30,7 +32,7 @@ endGame();
 //starts off the graveyard path
 function graveyard(){
     alert(`So,you have chosen to explore the graveyard.`);
-    alert(`As you enter the graveyard, you see a gravestone that says your name on it.`)
+    alert(`As you enter the graveyard, you see a gravestone that says ${name} on it.`)
     graveChoice = +prompt("Do you (1) explore your own gravestone, or (2) run away because you are scared?")
     if (graveChoice == 1){
         alert("You explore your gravstone, and are shocked to see that it has the correct birthyear. You get scared and try to leave, but feel something grasp at your foot. You turn to see an arm sticking out of the ground. The arm pulls you into the ground, and you rest peacefully in your grave for the rest of eternity.");
@@ -39,15 +41,15 @@ function graveyard(){
         alert("You turn to run away, but you trip and hit your head, causing you to fall unconscious. It appears your grave will be useful.");
     }
     else{
-        alert("It appears youdid not put a proper response, please try again.")
+        alert("It appears that you did not put a proper response, please try again.")
         graveyard();
     }
 }
 //starts off the church path
 function church(){
-    alert(`So ${name}, you hae chosen to explore the church`);
+    alert(`So ${name}, you have chosen to explore the church`);
     alert("You enter the churh, walking you hear a loud thud in the back of the church")
-    churchChoice = +prompt("Yu jump from how scared you are, do you (1) keep walking, or do you (2) run away.")
+    churchChoice = +prompt("Yu jump from how scared you are, do you (1) keep walking, (2) run away, or (3) take something off the floor and fight")
     church2Choice();
 }
 //gives the player the correct path based on what they chose
@@ -59,6 +61,9 @@ function choice(){
             church();
             
         }
+        else if (direction == 3) {
+            
+        }
         else {
             alert("Sorry, I don't think you can escape fate, try again.");
             main();
@@ -68,12 +73,12 @@ function choice(){
 //Gives final church choices
 function church2Choice(){
     if (churchChoice == 1){
-        alert(`You keep walking, you hear someone call you name, you black out, the next thing you know you are lying in a hospital with all your friends sitting worried.`)
+        alert(`You keep walking, you hear someone say ${name}, you black out, the next thing you know you are lying in a hospital with all your friends sitting worried.`)
         endGame();
     }
 
     else if (churchChoice == 2) {
-        alert(`You run out of the church, someone behind you yells out your name soon you see the exit of the graveyard, the door is closed, your friends left you.`)
+        alert(`You run out of the church, someone behind you yells ${name} soon you see the exit of the graveyard, the door is closed, your friends left you.`)
         endGame();
     }
     else {
@@ -84,6 +89,26 @@ function church2Choice(){
 //Function used to end the game
 function endGame(){
     alert("Thank you for playing!" );
+}
+function churchFight() {
+    weapon = prompt(`${name} what do you want to pickup from the self to fight? A shovel, flashlight, stick, broken laptop or a painting?`)
+    outcome = Math.round((Math.random() * 4) + 1);
+    if (outcome == 1) {
+        alert(`You pickup the ${weapon} and try to fight, but it snaps in half and you stand there, waiting for your end. The creatures leaps onto you and the next thing you know you are lying in the hospital bed, your friends surrounding you.`);
+        endGame();
+    }
+    else if (outcome == 2 {
+        alert(` You pickup the ${weapon} and attack, you hit the creature in the head and it falls down, you turn around and run away.`);
+        endGame();
+    }
+    else if (outcome == 3) {
+        alert(` You pickup the ${weapon} leaping onto the creature you hit the creature but nothing happens, he just growls. He picks you up and carries you over to a gravestone with your name writen on it. He throws you in and scratches todays date.`);
+        endGame();
+    }
+    else if (outcome == 4) {
+         alert(` You pickup the ${weapon} but then before you know it he leaps onto you, you hold out your ${weapon} and try to defend. The creature keeps saying ${name}, but you are lucky and are able to stab him, you run off and see that your friends ran off scared. You get home safely`);
+        endGame();
+    }
 }
 //Starts the game
 startGame();
